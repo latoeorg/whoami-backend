90de6bda8b27228f2bc5b4e0861450e33c5933e0
lazlanrafar <lazlanrafar@gmail.com>
2024-01-03 22:01:45 +0800

    initial project

---
+++ constant/env.ts
@@ -0,0 +1,3 @@
+export const ENV = {
+  API_PORT: process.env.API_PORT,
+};
---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { ENV } from "./constant";
+
+dotenv.config();
+
+const app: Express = express();
+
+app
+  .disable("x-powered-by")
+  .use(logger("dev"))
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, "public")));
+
+const port = ENV.API_PORT || 3000;
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
